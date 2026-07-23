ad4c8a918649a8769b882c18aa723ffcf052ef49
Developer <110857695+wfwegryrtu290@users.noreply.github.com>
2026-07-23 23:20:41 +0300

    Enhance EventBus with unsubscribe and improved emit
    Added unsubscribe method to remove listeners and updated emit method to log exceptions.

--- core/events.py
+++ core/events.py
@@ -1,3 +1,6 @@
+from core.logger import logger
+
+
 class EventBus:
 
     def __init__(self):
@@ -5,22 +8,69 @@ class EventBus:
         self.listeners = {}
 
     def subscribe(self, event, callback):
+        """
+        Регистрира слушател за събитие.
+        """
 
         if event not in self.listeners:
-
             self.listeners[event] = []
 
-        self.listeners[event].append(callback)
+        if callback not in self.listeners[event]:
+            self.listeners[event].append(callback)
 
-    def emit(self, event, data=None):
+    def unsubscribe(self, event, callback):
+        """
+        Премахва слушател.
+        """
 
         if event not in self.listeners:
-
             return
 
-        for callback in self.listeners[event]:
+        if callback in self.listeners[event]:
+            self.listeners[event].remove(callback)
 
-            callback(data)
+            if not self.listeners[event]:
+                del self.listeners[event]
+
+    def emit(self, event, data=None):
+        """
+        Изпраща събитие към всички слушатели.
+        """
+
+        listeners = self.listeners.get(event, [])
+
+        logger.debug(f"📡 Event: {event}")
+
+        for callback in listeners:
+
+            try:
+                callback(data)
+
+            except Exception as e:
+                logger.exception(e)
+
+    def clear(self):
+        """
+        Изчиства всички слушатели.
+        """
+
+        self.listeners.clear()
+
+    def has_event(self, event):
+
+        return event in self.listeners
+
+    def events(self):
+
+        return list(self.listeners.keys())
+
+    def count(self):
+
+        return len(self.listeners)
+
+    def listener_count(self, event):
+
+        return len(self.listeners.get(event, []))
 
 
 bus = EventBus()
