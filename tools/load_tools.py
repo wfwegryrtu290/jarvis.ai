cff09e6c417be316b2b5b8f23927506ea1ca79b0
Developer <110857695+wfwegryrtu290@users.noreply.github.com>
2026-07-24 18:18:54 +0300

    Create load_tools.py

---
+++ tools/load_tools.py
@@ -0,0 +1,8 @@
+from computer.opener import open_target
+from computer.reader import read_file
+from computer.lister import list_folder
+from computer.window import activate
+from computer.window import close
+
+# Ако по-късно добавиш още инструменти,
+# просто ги импортни тук.
